9d01cad08c883e0d724f354c2f7e02c560e5a993
Hunter Andersen <andersen.hunter42@gmail.com>
2023-04-19 06:52:13 -0700

    Finished example and added comments

--- script.js
+++ script.js
@@ -30,16 +30,53 @@ const users = [
   {
     name: "Lucius Best",
     powerLevel: 6,
-    age: 40,
+    age: 41,
     favColor: "lightblue",
     superName: "Frozone"
   }
 ];
 
+
+//Derived State: State that was derived from other state
+
 //Filter method
 //Only show the super heros with a powerLevel of 7 or greater
-//Creating a new array somehow to plug in down below
+//Create a new array somehow to plug in down below
+const filteredUsers = users.filter((element) => {
+    return (element.powerLevel >= 7);
+});
 
+//Map Method
+//Create a new array that has all the previous users,
+//but does not contain their powerLevel or age properties
+const mappedUsers = users.map((element) => {
+    console.log(element);
+    return {
+        name: element.name,
+        favColor: element.favColor,
+        superName: element.superName
+    }
+});
+console.log(mappedUsers);
+
+//A few ways to copy an array in order to prevent mutating the original array
+let copiedArr = users.slice(0);
+copiedArr = [...users];
+copiedArr = Array.from(users);
+//"Array" is a built-in class that has static methods
+//available on it for you to use
+
+const sortedUsers = copiedArr.sort((a, b) => {
+    // return b.powerLevel - a.powerLevel;
+    if (b.powerLevel > a.powerLevel){
+        return 1;
+    } else if (a.powerLevel > b.powerLevel){
+        return -1;
+    }else {
+        return a.age - b.age;
+    }
+
+});
 
 function renderCardsData(data){
     const placeholder = document.querySelector("#placeholder");
@@ -61,11 +98,11 @@ function renderCardsData(data){
             }
         });
 
-
         placeholder.appendChild(superCard);
     });
     
 }
 
-//Changing this variable
-renderCardsData(users);
+//Changing this variable changes what gets displayed on the page
+//We can choose any of our derived state to render instead of the whole original array
+renderCardsData(sortedUsers);
